6cc1aef057b534ef1c1fc7e7a0d744d8e27854b8
wizche <sergio@geeksonsoftware.com>
2014-06-04 11:52:52 +0100

    Removed environment dependant configurations, added properties caching

--- src/main/java/com/geeksonsoftware/mineboard/agent/service/StaticDataService.java
+++ src/main/java/com/geeksonsoftware/mineboard/agent/service/StaticDataService.java
@@ -4,35 +4,47 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 public class StaticDataService {
 
-	public static String getWebsiteUrl() {
-		Properties prop = new Properties();
-		InputStream input = null;
+	public static int DEFAULT_TIME_INTERVAL = 15;
+	private static Properties properties = null;
+	private static Logger log = Logger.getLogger(StaticDataService.class);
 
-		try {
-			input = StaticDataService.class
-					.getResourceAsStream("/config.dev.properties");
-			if (input != null) {
-				prop.load(input);
-				String url = prop.getProperty("url");
-				return url != null ? url
-						: "http://localhost:3000/api/dashboard/";
-			}
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
+	public static String getWebsiteUrl() {
+		if (System.getProperty("dev") != null) {
+			log.debug("Development mode, returning local URL");
+			return "http://localhost:3000/api/dashboard/";
+		}
+		String url = getProperties().getProperty("url");
+		return url != null ? url : "https://mineboard.io/api/dashboard/";
+	}
+
+	private static Properties getProperties() {
+		if (properties == null) {
+
+			properties = new Properties();
+			InputStream input = null;
+
+			try {
+				input = StaticDataService.class
+						.getResourceAsStream("/config.properties");
+				if (input != null) {
+					properties.load(input);
+				}
+			} catch (IOException ex) {
+				log.error("Unable to load properties!", ex);
+			} finally {
+				if (input != null) {
+					try {
+						input.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}
-		return "http://localhost:3000/api/dashboard/";
+		return properties;
 	}
-
-	public static int DEFAULT_TIME_INTERVAL = 15;
-
 }
